261734fb05b50d1b901480f175d0a4a62719e5cd
Argyris <arkatsy@gmail.com>
2024-03-19 04:46:49 +0200

    feat: add credits

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function getInitWordFromUrl() {
   return url.searchParams.get("word");
 }
 
+// TODO: Move providers to main.tsx
 export default function App() {
   const initWord = getInitWordFromUrl();
 
@@ -33,7 +34,41 @@ export default function App() {
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex items-center justify-center">
-      <div className="mx-5 flex w-full max-w-4xl flex-col gap-10 py-14">{children}</div>
+      <div className="relative mx-5 w-full max-w-4xl py-14">
+        <div className="flex flex-col gap-10">{children}</div>
+        <Credits />
+      </div>
+    </div>
+  );
+}
+
+// TODO: Find a better place to show the credits.
+function Credits() {
+  return (
+    <div className="absolute bottom-2 flex gap-3">
+      <div className="flex gap-2">
+        <p>Created by</p>
+        <a
+          href="https://github.com/arkatsy"
+          target="_blank"
+          rel="noreferrer"
+          className="text-red-500 underline underline-offset-2 dark:text-red-400"
+        >
+          @arkatsy
+        </a>
+      </div>
+      &#8226;
+      <div className="flex gap-2">
+        <p>API by</p>
+        <a
+          href="https://dictionaryapi.dev/"
+          target="_blank"
+          rel="noreferrer"
+          className="text-violet-500 underline underline-offset-2"
+        >
+          dictionaryapi.dev
+        </a>
+      </div>
     </div>
   );
 }
