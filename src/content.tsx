9dc1153199ad0c0ba410921ecc6e065c02c2dcfe
Argyris <arkatsy@gmail.com>
2024-03-19 09:19:29 +0200

    feat: add spinner

--- src/content.tsx
+++ src/content.tsx
@@ -8,7 +8,12 @@ export default function Content() {
   const [playing, setPlaying] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading)
+    return (
+      <div className="flex justify-center">
+        <Spinner />
+      </div>
+    );
 
   // TODO: Differentiate between an API error and word not found error. (see dictionary-provider.tsx)
   if (error) return <div>{error}</div>;
@@ -151,6 +156,25 @@ function PauseIcon() {
       className="size-8 fill-current text-violet-700 dark:text-violet-300"
     >
       <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
+    </svg>
+  );
+}
+
+function Spinner() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="size-8 animate-spin text-violet-500 dark:text-violet-400"
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
     </svg>
   );
 }
